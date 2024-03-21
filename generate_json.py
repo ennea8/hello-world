78877f603400647ab57d8d7c0de0ab47376330f7
ennea8 <ennea8@gmail.com>
2024-03-21 20:11:12 +0800

    script:generate json data

---
+++ generate_json.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+import posixpath
+from urllib.parse import quote
+import re
+import json
+
+def regexReplace(string, search, replacement):
+    return re.compile(search).sub(replacement, string)
+
+json_data = {}
+
+languageCount = 0
+languagesText = ""
+content=""
+
+# List the available languages
+for directory in sorted(os.listdir('.')):
+    if not (directory == '.' or directory == '..' or directory[0] == '.'
+            or os.path.isfile(directory)):
+        for filename in sorted(os.listdir(directory), key=lambda s: s.lower()):
+            if os.path.isfile(os.path.join(directory, filename)):
+                language = (os.path.splitext(filename)[0].replace(
+                    "-", "-").replace("∕", "/").replace("＼", "\\").replace(
+                        "˸", ":").replace("∗", "*").replace("？", "?").replace(
+                            "＂",
+                            "\"").replace("﹤",
+                                          "<").replace("﹥",
+                                                       ">").replace("❘", "|"))
+                languagesText += f'* [{language}]({posixpath.join(quote(directory), quote(filename))})\n'
+                languageCount += 1
+
+                json_data[languageCount]={}
+                json_data[languageCount]['language'] = language
+                json_data[languageCount]['path'] = f'{posixpath.join(quote(directory), quote(filename))}'
+
+                with open(os.path.join(directory, filename), 'r', encoding='utf-8', errors='ignore') as file:
+                    try:
+                        content = file.read()
+                    except UnicodeDecodeError:
+                        content = file.read().decode('latin-1')
+                    json_data[languageCount]['content'] = content
+
+file_path = 'data.json'
+with open(file_path, 'w') as json_file:
+    json.dump(json_data, json_file, indent=4)
